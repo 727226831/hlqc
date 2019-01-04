02e3736bdd511a4c2fe31e3e71dceb2fee2941be
curry <i13166380861@163.com>
2019-01-04 10:57:11 +0800

    Initial commit

---
+++ src/main/java/com/example/storescontrol/BaseActivity.java
@@ -0,0 +1,40 @@
+package com.example.storescontrol;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+
+public class BaseActivity extends AppCompatActivity {
+    public static   String acccode,usercode;
+    public  AlertDialog dialog;
+    public  SharedPreferences sharedPreferences;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        AlertDialog.Builder builder=new AlertDialog.Builder(this).setView(R.layout.layout_progress);
+        dialog=builder.create();
+
+
+
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/example/storescontrol/PortActivity.java
@@ -0,0 +1,71 @@
+package com.example.storescontrol;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.storescontrol.Url.Request;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.URL;
+
+
+public class PortActivity extends BaseActivity{
+    TextView titleTv;
+    private Button buttonok;
+    private EditText editText;
+    private ImageButton imageButton;
+    SharedPreferences sharedPreferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_port);
+        titleTv=findViewById(R.id.tv_title);
+        titleTv.setText("设置");
+        buttonok=findViewById(R.id.b_ok);
+        editText=findViewById(R.id.et_port);
+        imageButton=findViewById(R.id.iv_return);
+        imageButton.setVisibility(View.VISIBLE);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+         sharedPreferences= getSharedPreferences("sp", Context.MODE_PRIVATE);
+
+         editText.setText(sharedPreferences.getString("port",""));
+
+        buttonok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!editText.getText().toString().equals("")&& Patterns.WEB_URL.matcher(editText.getText().toString()).matches()){
+                    Request.URL=editText.getText().toString();
+                    SharedPreferences.Editor editor = sharedPreferences.edit();//获取编辑器
+                    editor.putString("port",editText.getText().toString());
+                    editor.commit();
+                    Toast.makeText(PortActivity.this,"地址已改变",Toast.LENGTH_LONG).show();
+                    finish();
+                }else {
+                    Toast.makeText(PortActivity.this,"地址不正确",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
+    }
+
+}
